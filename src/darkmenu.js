87d2f0520878b31218f965a770913815570607ed
foxgiant157 <77528847+foxgiant157@users.noreply.github.com>
2021-01-16 09:52:13 -0300

    Add files via upload

---
+++ src/darkmenu.js
@@ -0,0 +1,41 @@
+const darkmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Fox:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *FOX*
+  DUVIDAS? 👇
+  WA.me/5527999343206
+╚════════════════════`
+}
+
+exports.darkmenu = darkmenu
+
+
+
+
+
+
+
+
+
